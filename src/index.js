46320f12f22cf1f665ef2e336d8f5953e4282c56
Vlad0703 <73859766+Vlad0703@users.noreply.github.com>
2021-02-27 21:25:19 +0300

    Update index.js
    fix: fix handling empty string

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    let tow = matrix;
-    for (let i = 1; i < tow.length; i+=2 ) {
+  tow = matrix;
+  if (typeof tow === 'undefined') {
+    return tow = [];
+  } else {
+    for (let i = 1; i < tow.length; i += 2) {
       tow[i].reverse();
     }
     return tow.flat();
+  }
 }
